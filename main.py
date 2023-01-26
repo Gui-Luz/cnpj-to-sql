f31e6e0fea8c46d5d57c496ead709be23b61467e
Gui-Luz <guilhermeluzsaldanha@gmail.com>
2023-01-26 20:27:05 -0300

    correction in configparser

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import configparser
 import os
-from src.models.zip_downloader import ZipDownloader
-from src.models.postgres_connector import PostgresConnector
-from src.models.csv_handler import CsvHandler
-from src.helpers.ascii_printer import ascii_printer
 
 config_file = configparser.ConfigParser()
-config_file.read(os.path.dirname(__file__) + '/config.ini')
+config_file.read('./config.ini')
 
 base_url = config_file['DOWNLOAD']['base_url']
 download_path = config_file['DOWNLOAD']['zip_path']
@@ -23,17 +19,17 @@ if __name__ == '__main__':
         art = f.read()
     print(art)
 
-    zip = ZipDownloader(base_url, download_path)
-    zip.get_zip_objects()
-    zip.get_files_length()
-    zip.get_downloaded_files_length()
-    zip.download_zip_files()
-
-    ch = CsvHandler(download_path, csv_path)
-    ch.unzip_files()
-    ch.encoding_converter()
-    ch.remove_null_bytes()
-
-    post = PostgresConnector(postgres_host, postgres_database, postgres_user, postgres_password, csv_path)
-    post.create_tables()
-    post.load_postgres()
+    # zip = ZipDownloader(base_url, download_path)
+    # zip.get_zip_objects()
+    # zip.get_files_length()
+    # zip.get_downloaded_files_length()
+    # zip.download_zip_files()
+    #
+    # ch = CsvHandler(download_path, csv_path)
+    # ch.unzip_files()
+    # ch.encoding_converter()
+    # ch.remove_null_bytes()
+    #
+    # post = PostgresConnector(postgres_host, postgres_database, postgres_user, postgres_password, csv_path)
+    # post.create_tables()
+    # post.load_postgres()
--- src/helpers/ascii_printer.py
+++
@@ -1,4 +0,0 @@
-def ascii_printer():
-    with open("/logo/logo.logo", "r") as f:
-        art = f.read()
-    print(art)
