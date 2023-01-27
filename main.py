d030c9aeea02bb1724b18a52b5b23b702cc048e2
Gui-Luz <guilhermeluzsaldanha@gmail.com>
2023-01-27 16:57:54 -0300

    add requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import configparser
-import os
+from src.models.zip_downloader import ZipDownloader
+from src.models.csv_handler import CsvHandler
+from src.models.postgres_connector import PostgresConnector
 
 config_file = configparser.ConfigParser()
 config_file.read('./config.ini')
@@ -19,17 +21,17 @@ if __name__ == '__main__':
         art = f.read()
     print(art)
 
-    # zip = ZipDownloader(base_url, download_path)
-    # zip.get_zip_objects()
-    # zip.get_files_length()
-    # zip.get_downloaded_files_length()
-    # zip.download_zip_files()
-    #
-    # ch = CsvHandler(download_path, csv_path)
-    # ch.unzip_files()
-    # ch.encoding_converter()
-    # ch.remove_null_bytes()
-    #
-    # post = PostgresConnector(postgres_host, postgres_database, postgres_user, postgres_password, csv_path)
-    # post.create_tables()
-    # post.load_postgres()
+    zip = ZipDownloader(base_url, download_path, csv_path)
+    zip.get_zip_objects()
+    zip.get_files_length()
+    zip.get_downloaded_files_length()
+    zip.download_zip_files()
+
+    ch = CsvHandler(download_path, csv_path)
+    ch.unzip_files()
+    ch.encoding_converter()
+    ch.remove_null_bytes()
+
+    post = PostgresConnector(postgres_host, postgres_database, postgres_user, postgres_password, csv_path)
+    post.create_tables()
+    post.load_postgres()
--- src/models/encoding_converter.py
+++ src/models/encoding_converter.py
@@ -6,7 +6,7 @@ class EncodingConverter:
     @classmethod
     def convert(cls, file):
         print(f'[+] {datetime.now()} - Converting {file}')
-        with open(file, 'r', encoding='logo', errors='replace') as f_input:
+        with open(file, 'r', encoding='latin-1', errors='replace') as f_input:
             contents = f_input.read()
 
         with open(file, 'w', encoding='utf-8', errors='replace') as f_output:
